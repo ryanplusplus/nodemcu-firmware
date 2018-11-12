580b028c43d439599cf814a3e8b2b9ad50eb5d41
Ryan Hartlage <ryanplusplus@gmail.com>
2018-11-12 15:42:33 -0500

    Remove extra installation of GPIO ISR service; configure pinout for M5Stack camera

--- components/modules/camera.c
+++ components/modules/camera.c
@@ -14,27 +14,24 @@ static int lcamera_init(lua_State *L)
 {
   esp_err_t err;
 
-  // no idea if this is necessary
-  ESP_ERROR_CHECK(gpio_install_isr_service(0));
-
   camera_config_t camera_config = {
     .ledc_channel = LEDC_CHANNEL_0,
     .ledc_timer = LEDC_TIMER_0,
-    .pin_d0 = 4,
-    .pin_d1 = 5,
-    .pin_d2 = 18,
-    .pin_d3 = 19,
-    .pin_d4 = 36,
-    .pin_d5 = 39,
-    .pin_d6 = 34,
-    .pin_d7 = 35,
-    .pin_xclk = 21,
-    .pin_pclk = 22,
-    .pin_vsync = 25,
-    .pin_href = 23,
-    .pin_sscb_sda = 26,
-    .pin_sscb_scl = 27,
-    .pin_reset = 2,
+    .pin_d0 = 17,
+    .pin_d1 = 35,
+    .pin_d2 = 34,
+    .pin_d3 = 5,
+    .pin_d4 = 39,
+    .pin_d5 = 18,
+    .pin_d6 = 36,
+    .pin_d7 = 19,
+    .pin_xclk = 27,
+    .pin_pclk = 21,
+    .pin_vsync = 22,
+    .pin_href = 26,
+    .pin_sscb_sda = 25,
+    .pin_sscb_scl = 23,
+    .pin_reset = 15,
     .xclk_freq_hz = 20000000
   };
 
